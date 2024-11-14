56eafd51ed6a7a1eada2686ec1aecec6a2579a94
microdotmatrix <liquid.knives.2@gmail.com>
2024-11-14 16:37:11 -0600

    Revisions of tagline, team bios, list items, mission statement; add new company links, move down testimonials

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,10 +1,19 @@
+// Export all icons as named exports
+
+export { default as Assembly } from "./assembly.astro";
+export { default as Battery } from "./battery.astro";
 export { default as Copyright } from "./copyright.astro";
 export { default as Design } from "./design.astro";
 export { default as Facebook } from "./facebook.astro";
+export { default as Factory } from "./factory.astro";
 export { default as Innovation } from "./innovation.astro";
 export { default as Instagram } from "./instagram.astro";
+export { default as LaserTool } from "./laser-tool.astro";
 export { default as LinkedIn } from "./linkedin.astro";
 export { default as Manufacturing } from "./manufacturing.astro";
+export { default as Package } from "./package.astro";
+export { default as Precision } from "./precision.astro";
 export { default as Referal } from "./referal.astro";
+export { default as RobotArm } from "./robot-arm.astro";
 export { default as Source } from "./source.astro";
 export { default as X } from "./x.astro";
